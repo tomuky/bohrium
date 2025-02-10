56334b156295aa1ff99342cf5509b45c1056b006
Tom <mrtomd@gmail.com>
2025-02-10 12:43:30 -0500

    socials on splash

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -19,13 +19,13 @@ export default function Home() {
         </div>
         <div className={styles.socialLinks}>
           <Link href="https://x.com/bohrsupply" target="_blank">
-            <Image src="/images/x.png" alt="X" width={24} height={24}/> 
+            <Image src="/images/x.png" alt="X" width={24} height={24} style={{ filter: 'invert(1)' }}/> 
           </Link>
           <Link href="https://discord.gg/xyZW4Ck36V" target="_blank">
             <Image src="/images/discord.png" alt="Discord" width={30} height={24}/>
           </Link>
           <Link href="https://github.com/tomuky/bohrium" target="_blank">
-            <Image src="/images/github.png" alt="GitHub" width={24} height={24}/>
+            <Image src="/images/github.png" alt="GitHub" width={24} height={24} style={{ filter: 'invert(1)' }}/>
           </Link>
         </div>
       </nav>
