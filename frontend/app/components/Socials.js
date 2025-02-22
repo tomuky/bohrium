7ca0d042d10e46afa10a37a0c52230637f21984b
Tom <mrtomd@gmail.com>
2025-02-22 13:18:13 -0500

    open links in new tabs

--- frontend/app/components/Socials.js
+++ frontend/app/components/Socials.js
@@ -6,17 +6,17 @@ const Socials = () => {
     return (
         <div className={`${styles.area} ${styles.desktopOnly}`}>
             <div className={styles.item}>
-                <Link href="https://discord.gg/xyZW4Ck36V">
+                <Link href="https://discord.gg/xyZW4Ck36V" target="_blank" rel="noopener noreferrer">
                     <Image src="/images/discord.png" alt="Discord" width={30} height={24} className={styles.icon} />
                 </Link>
             </div>
             <div className={styles.item}>
-                <Link href="https://github.com/tomuky/bohrium">
+                <Link href="https://github.com/tomuky/bohrium" target="_blank" rel="noopener noreferrer">
                     <Image src="/images/github.png" alt="Github" width={24} height={24} className={styles.icon} />
                 </Link>
             </div>
             <div className={styles.item}>
-                <Link href="https://x.com/bohrsupply">
+                <Link href="https://x.com/bohrsupply" target="_blank" rel="noopener noreferrer">
                     <Image src="/images/x.png" alt="Twitter" width={24} height={24} className={styles.icon} />
                 </Link>
             </div>
