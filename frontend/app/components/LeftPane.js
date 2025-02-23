d86b35d79bd94adb014afe650a468100b7fba60f
Tom <mrtomd@gmail.com>
2025-02-23 11:35:27 -0500

    hide left panel if no session wallet yet

--- frontend/app/components/LeftPane.js
+++ frontend/app/components/LeftPane.js
@@ -2,13 +2,17 @@
 import styles from './LeftPane.module.css'
 import Account from './Account'
 import Socials from './Socials'
+import { useSessionWallet } from '../contexts/SessionWalletContext'
+
 const LeftPane = () => {
+    const { hasSessionWallet } = useSessionWallet();
+
     return (
         <div className={styles.leftPane}>
             <div className={styles.testnet}>
                 <p>Base Sepolia Testnet Only</p>
             </div>
-            <Account />
+            {hasSessionWallet && <Account />}
             <Socials />
         </div>
     )
